ce8281ad69497bb3eb6555b4402502f5d55ed245
qninhdt <qndt123@gmail.com>
2023-07-18 12:23:24 +0700

    Response JSON file

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import flask
+import json
 from flask import request
 from core.quiz_converter import QuizConverter
 
@@ -21,10 +22,49 @@ def quiz2quiz():
 
     # convert docx to JSON
     quiz_converter = QuizConverter(file)
-    return quiz_converter.get_json()
+    json_data = quiz_converter.get_json()
+
+    with open(f'data/{quiz_converter.id}/questions.json', 'w') as f:
+        json_questions = {
+            'questions': []
+        }
+
+        for question in json_data['questions']:
+
+            json_questions['questions'].append({
+                'content': question['content'],
+                'choices': question['choices']
+            })
+
+        json.dump(json_questions, f)
+
+    with open(f'data/{quiz_converter.id}/answers.json', 'w') as f:
+        json_answers = {
+            'answers': []
+        }
+
+        for question in json_data['questions']:
+            json_answers['answers'].append(question['answer'])
+
+        json.dump(json_answers, f)
+
+    return {
+        'testid': quiz_converter.id,
+        'num_questions': len(json_data['questions'])
+    }
 
 
-@app.get('/media/<id>/<filename>')
+@app.get('/data/<id>/questions')
+def questions(id):
+    return flask.send_from_directory(f'data/{id}', 'questions.json')
+
+
+@app.get('/data/<id>/answers')
+def answers(id):
+    return flask.send_from_directory(f'data/{id}', 'answers.json')
+
+
+@app.get('/data/<id>/media/<filename>')
 def media(id, filename):
     return flask.send_from_directory(f'data/{id}/word/media', filename)
 

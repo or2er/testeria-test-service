2bced09337fa389859aa0fbd5af04a3768eb70df
qninhdt <qndt123@gmail.com>
2023-07-20 12:45:15 +0700

    Update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from core.quiz_converter import QuizConverter
 
-converter = QuizConverter('assets/example.docx')
+converter = QuizConverter('assets/math.docx')
 
 print(converter.get_json())
 print(converter.id)
